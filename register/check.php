0ce7b69515a77b080cb00bd874104ac7dd49e9de
上原康平 <>
2019-03-05 15:57:49 +0800

    no message

--- register/check.php
+++ register/check.php
@@ -1,48 +1,69 @@
 <?php
+session_start();
+
+// 不正遷移制御
+// sign.phpから来ていない場合、signup.phpに強制遷移させる
+if (!isset($_SESSION['51_LearnSNS'])) {
+    header('Location: signup.php');
+    exit();
+}
+
+// echo '<pre>';
+// var_dump($_SESSION);
+// echo '</pre>';
+
+$name = $_SESSION['51_LearnSNS']['name'];
+$email = $_SESSION['51_LearnSNS']['email'];
+$password = $_SESSION['51_LearnSNS']['password'];
+$img_name = $_SESSION['51_LearnSNS']['img_name'];
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
 <head>
-    <meta charset="utf-8">
-    <title>Learn SNS</title>
-    <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
-    <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
+   <meta charset="utf-8">
+   <title>Learn SNS</title>
+   <link rel="stylesheet" type="text/css" href="../assets/css/bootstrap.css">
+   <link rel="stylesheet" type="text/css" href="../assets/font-awesome/css/font-awesome.css">
 </head>
 <body style="margin-top: 60px">
-    <div class="container">
-        <div class="row">
-            <div class="col-xs-8 col-xs-offset-2 thumbnail">
-                <h2 class="text-center content_header">アカウント情報確認</h2>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <img src="../user_profile_img/misae.png" class="img-responsive img-thumbnail">
-                    </div>
-                    <div class="col-xs-8">
-                        <div>
-                            <span>ユーザー名</span>
-                            <p class="lead">野原みさえ</p>
-                        </div>
-                        <div>
-                            <span>メールアドレス</span>
-                            <p class="lead">misae@nohara.com</p>
-                        </div>
-                        <div>
-                            <span>パスワード</span>
-                            <p class="lead">●●●●●●●●</p>
-                        </div>
-                        <form method="POST" action="thanks.php">
-                            <a href="signup.php?action=rewrite" class="btn btn-default">&laquo;&nbsp;戻る</a> | 
-                            <input type="hidden" name="action" value="submit">
-                            <input type="submit" class="btn btn-primary" value="ユーザー登録">
-                        </form>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <script src="../assets/js/jquery-3.1.1.js"></script>
-    <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
-    <script src="../assets/js/bootstrap.js"></script>
+   <div class="container">
+       <div class="row">
+           <div class="col-xs-8 col-xs-offset-2 thumbnail">
+               <h2 class="text-center content_header">アカウント情報確認</h2>
+               <div class="row">
+                   <div class="col-xs-4">
+                       <img src="../user_profile_img/<?php echo $img_name; ?>" class="img-responsive img-thumbnail">
+                   </div>
+                   <div class="col-xs-8">
+                       <div>
+                           <span>ユーザー名</span>
+                           <p class="lead"><?php echo htmlspecialchars($name); ?></p>
+                       </div>
+                       <div>
+                           <span>メールアドレス</span>
+                           <p class="lead"><?php echo htmlspecialchars($email); ?></p>
+                       </div>
+                       <div>
+                           <span>パスワード</span>
+                           <p class="lead">●●●●●●●●</p>
+                       </div>
+                       <form method="POST" action="thanks.php">
+                           <a href="signup.php?action=rewrite" class="btn btn-default">&laquo;&nbsp;戻る</a> |
+                           <input type="hidden" name="action" value="submit">
+                           <input type="submit" class="btn btn-primary" value="ユーザー登録">
+                       </form>
+                   </div>
+               </div>
+           </div>
+       </div>
+   </div>
+   <script src="../assets/js/jquery-3.1.1.js"></script>
+   <script src="../assets/js/jquery-migrate-1.4.1.js"></script>
+   <script src="../assets/js/bootstrap.js"></script>
 </body>
 </html>
+メッセージ入力
+
+
+daiki nakashio へのメッセージ
--- register/signup.php
+++ register/signup.php
@@ -1,11 +1,27 @@
 <?php
+session_start();
+
 //バリデーション
 //入力が正しく設定されているか確認し、不正な場合はユーザーに再入力/再選択を促す機能
 
 //何か不備があった時に内容を格納する配列
 $errors = [];
 
+// check.phpから「戻る」できた場合
+if (isset($_GET['action']) && $_GET['action'] == 'rewrite') {
+    $_POST['input_name'] = $_SESSION['51_LearnSNS']['name'];
+    $_POST['input_email'] = $_SESSION['51_LearnSNS']['email'];
+    $_POST['input_password'] = $_SESSION['51_LearnSNS']['password'];
+    //書き直すために戻ってきたという不備として扱う
+    $errors['rewrite'] = true;
+}
+
+$name = '';
+$email = '';
 //POST送信時
+//empty(配列)
+//指定された配列が空の場合true, 空ではない場合false
+//!は論理は反映させる
 if (!empty($_POST)) {
     //$_POST
     //スーパーグローバル変数
@@ -50,15 +66,78 @@ if (!empty($_POST)) {
     // $_FILES[キー]['tmp_name']データそのもの
 
     //画像の取得
-    $file_name = $_FILES['input_img_name']['name'];
+    $file_name = '';
+    if (!isset($_GET['action'])) {
+        $file_name = $_FILES['input_img_name']['name'];
+    }
     //画像名か空かどうかチェック　＝ 画像が未選択かチェック
     if ($file_name != '') {
         //画像が選択されていたとき
+
+        //拡張子のチェック
+        //substr(文字列、開始1)
+        //指定した文字列のn文字目から文字を取得する
+        //$str =substr('abcdefghi' 0)
+        //echgi $str => defghi
+        //$str = substr('abcdefghi', -1);
+        //echo $str; => i
+
+        $file_type = substr($file_name, -3);
+
+        //大文字は全て小文字化
+        //strtolower(文字列)
+        //指定された文字列を小文字に変換する
+
+        $file_type = strtolower($file_type);
+
+        if ($file_type != 'jpg' && $file_type != 'png' && $file_type != 'gif') {
+            $errors['img_name'] = 'type';
+        }
     } else {
         //g画像が選択の時
         $errors['img_name'] = 'blank';
     }
 }
+
+   //バリエーション全て通過時
+   if (empty($errors)) {
+       //画像のアップロード
+       //画像自体はフォルダに保存
+       //画像のパスはDBに保存
+
+       //一意のファイル名を生成
+       //20190300514052529hoge.png
+       //現在の日時分秒を取得
+       //date(フォーマート)
+       //指定されたフォーマートで現在の日時を取得する
+       $date_str = date('YmdHis');
+       //タイムスタンプとファイル名を結合して一意のファイル名を生成する
+       $submit_file_name = $date_str.$file_name;
+
+       //アップロード処理
+       // move_uploaded_file(ファイル、アップロード先)
+       move_uploaded_file($_FILES['input_img_name']['tmp_name'], '../user_profile_img/'.$submit_file_name);
+       // ../は１個上のフォルダという意味
+
+       //セッションに保存されたデータを保存
+       //セッションとは
+       //各サーバーに用意された一時的にデータを保存する機能
+       //同一サーバー内であれば横断的に利用することができる
+       //PHPでは$_SESSIONを利用する
+       //使用ルール
+       //   ファイルの先頭にsession_start()を記述する
+
+       //セッションに値を格納
+       //連想配列形式
+       $_SESSION['51_LearnSNS']['name'] = $name;
+       $_SESSION['51_LearnSNS']['email'] = $email;
+       $_SESSION['51_LearnSNS']['password'] = $password;
+       $_SESSION['51_LearnSNS']['img_name'] = $submit_file_name;
+
+       // 画面遷移
+       header('Location: check.php');
+       exit();
+   }
 
 ?>
 <!DOCTYPE html>
@@ -126,6 +205,11 @@ if (!empty($_POST)) {
                             パスワードは　４〜１６文字で入力してください </p>
                         <?php endif; ?>
 
+                        <?php if (!empty($errors)): ?>
+                        <p class="test-danger">
+                        パスワードを再度入力してください</p>
+                        <?php endif; ?>
+
                     <div class="form-group">
                         <label for="img_name">プロフィール画像</label>
                         <input type="file" name="input_img_name" id="img_name" accept="image/*">
@@ -134,6 +218,11 @@ if (!empty($_POST)) {
                             画像を選択してください </p>
                         <?php endif; ?>
 
+                        <?php if (isset($errors['img_name']) && $errors['img_name'] == 'type'): ?>
+                            <p class="text-danger">
+                            拡張子がjpg,png、gifの画像を選択してください </p>
+                        <?php endif; ?>
+                        
                     </div>
                     <input type="submit" class="btn btn-default" value="確認">
                     <span style="float: right; padding-top: 6px;">ログインは
